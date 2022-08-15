94380a492bc8a71954409bc50525fedf2241e75c
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-08-15 23:11:55 +0500

    added hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
+import Hero from "layouts/Hero";
 import React from "react";
 
 function App() {
   return (
     <div>
-      <p className="">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga inventore
-        minima libero ut, nobis ipsam? Exercitationem fuga consequatur
-        repudiandae quasi nemo quam explicabo tenetur impedit quaerat
-        perferendis! Optio, cupiditate unde?
-      </p>
+      <Hero />
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import { MouseEventHandler } from "react";
 import styles from "scss/components/Button.module.scss";
 
-function Button() {
-  return <div>Button</div>;
+interface ButtonProps {
+  title: string;
+  variant?: "pink" | "transparent";
+  onClick?: MouseEventHandler | undefined;
+}
+
+function Button(props: ButtonProps) {
+  const { variant = "transparent", onClick, title } = props;
+
+  return (
+    <button
+      className={`fs-14px white weight-7 ${styles.button} ${
+        variant === "transparent" ? null : styles.pink
+      }`}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+  );
 }
 
 export default Button;
