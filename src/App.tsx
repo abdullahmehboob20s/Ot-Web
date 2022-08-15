300c83a0fa98182721e41161a75316e08bebaa99
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-08-15 22:37:56 +0500

    fixed unstable line-height of Axiforma font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <p className="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga inventore
+        minima libero ut, nobis ipsam? Exercitationem fuga consequatur
+        repudiandae quasi nemo quam explicabo tenetur impedit quaerat
+        perferendis! Optio, cupiditate unde?
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
