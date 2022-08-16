0b7137d3b358c6cd6afc6ce8cea7f5c1114b31db
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-08-16 09:27:39 +0500

    asSeen layout made for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import AsSeen from "layouts/AsSeen";
 import Hero from "layouts/Hero";
 import React from "react";
 
 function App() {
   return (
     <div>
-      <Hero />
+      <div className="mb-50px">
+        <Hero />
+      </div>
+      <div className="mb-100px">
+        <AsSeen />
+      </div>
     </div>
   );
 }
