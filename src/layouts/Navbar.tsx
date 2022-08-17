fa99a8fe5ace066d0bd56b8ba64b16a667e56462
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-08-17 17:28:42 +0500

    Fixed Overflow issue

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -23,80 +23,72 @@ function Navbar() {
   });
 
   return (
-    <>
-      <div
-        className={`${styles.navbar} ${isScrolled ? styles.scrolled : null}`}
-      >
-        <div className="container-wrapper-2">
-          <main>
-            <p className="fs-14px white weight-5">logo placeholder</p>
+    <div className={`${styles.navbar} ${isScrolled ? styles.scrolled : null}`}>
+      <div className="container-wrapper-2">
+        <main>
+          <p className="fs-14px white weight-5">logo placeholder</p>
 
+          <button
+            className={styles.hamburger}
+            onClick={() => setIsSidebarOpen((val) => !val)}
+          >
+            <IoMenu />
+          </button>
+
+          <div
+            ref={sidebarRef}
+            className={`${styles.links} ${isSidebarOpen ? styles.open : null}`}
+          >
             <button
-              className={styles.hamburger}
+              className={styles.closeIcon}
               onClick={() => setIsSidebarOpen((val) => !val)}
             >
-              <IoMenu />
+              <IoClose color="white" />
             </button>
-
-            <div
-              ref={sidebarRef}
-              className={`${styles.links} ${
-                isSidebarOpen ? styles.open : null
-              }`}
+            <Link to="home" className="fs-14px white weight-5 pointer">
+              Home
+            </Link>
+            <Link
+              to="aboutus"
+              className="fs-14px white weight-5 pointer"
+              offset={-160}
             >
-              <button
-                className={styles.closeIcon}
-                onClick={() => setIsSidebarOpen((val) => !val)}
-              >
-                <IoClose color="white" />
-              </button>
-              <Link to="home" className="fs-14px white weight-5 pointer">
-                Home
-              </Link>
-              <Link
-                to="aboutus"
-                className="fs-14px white weight-5 pointer"
-                offset={-160}
-              >
-                About Us
-              </Link>
-              <Link
-                to="tokenomics"
-                className="fs-14px white weight-5 pointer"
-                offset={-160}
-              >
-                Tokenomics
-              </Link>
-              <Link
-                to="faq"
-                className="fs-14px white weight-5 pointer"
-                offset={-160}
-              >
-                Faq
-              </Link>
-              <Link
-                to="calculator"
-                className="fs-14px white weight-5 pointer"
-                offset={-160}
-              >
-                Calculator
-              </Link>
-              <Link
-                to="roadmap"
-                className="fs-14px white weight-5 pointer"
-                offset={-160}
-              >
-                Roadmap
-              </Link>
-            </div>
+              About Us
+            </Link>
+            <Link
+              to="tokenomics"
+              className="fs-14px white weight-5 pointer"
+              offset={-160}
+            >
+              Tokenomics
+            </Link>
+            <Link
+              to="faq"
+              className="fs-14px white weight-5 pointer"
+              offset={-160}
+            >
+              Faq
+            </Link>
+            <Link
+              to="calculator"
+              className="fs-14px white weight-5 pointer"
+              offset={-160}
+            >
+              Calculator
+            </Link>
+            <Link
+              to="roadmap"
+              className="fs-14px white weight-5 pointer"
+              offset={-160}
+            >
+              Roadmap
+            </Link>
+          </div>
 
-            <div
-              className={`black-screen ${isSidebarOpen ? "show" : ""}`}
-            ></div>
-          </main>
-        </div>
+          <div className={`black-screen ${isSidebarOpen ? "show" : ""}`}></div>
+        </main>
       </div>
-    </>
+    </div>
   );
 }
 
